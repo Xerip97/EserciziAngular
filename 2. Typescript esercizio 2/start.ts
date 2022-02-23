f1dbf4c11a0f5129584e3ea58d6ccc39f1490cec
Fabio Viola <fabioviola97@gmail.com>
2022-02-23 17:57:52 +0100

    Fix ex 2

--- 2. Typescript esercizio 2/start.ts
+++ 2. Typescript esercizio 2/start.ts
@@ -1,18 +1,77 @@
-import {Person, Address, role, Company, gender} from "../1. Typescript esercizio 1/start";
+import {
+  Person,
+  Address,
+  role,
+  Company,
+  gender,
+} from "../1. Typescript esercizio 1/start";
 
 class User implements Person {
-     id: number;
-     name: string;
-     surname: string;
-     age: number;
-     dateOfBirth: string;
-     address: Address;
-     role: role;
-     username: string;
-     profilePhotoUrl: string;
-     companies: Company[];
-     gender: gender;
-     getFullName(): string {
-          return `${this.name} ${this.surname}`;
-     };
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  dateOfBirth: string;
+  address: Address;
+  role: role;
+  username: string;
+  profilePhotoUrl: string;
+  companies: Company[];
+  gender: gender;
+  getFullName(): string {
+    return `${this.name} ${this.surname}`;
+  }
+
+  constructor(
+    id: number,
+    name: string,
+    surname: string,
+    age: number,
+    dateOfBirth: string,
+    address: Address,
+    role: role,
+    username: string,
+    profilePhotoUrl: string,
+    companies: Array<Company>,
+    gender: gender
+  ) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.age = age;
+    this.dateOfBirth = dateOfBirth;
+    this.address = { ...address };
+    this.role = role;
+    this.username = username;
+    this.profilePhotoUrl = profilePhotoUrl;
+    this.companies = [...companies];
+    companies.forEach((company) => {
+      company.location = { ...company.location };
+    });
+    this.gender = gender;
+  }
 }
+let user = new User(
+  1,
+  "Fabio",
+  "Viola",
+  24,
+  "16/10/1997",
+  { city: "Cianciana", street: "Via Carubia 30", postalCode: "92012" },
+  "student",
+  "Xerip",
+  "url",
+  [
+    {
+      id: 148979,
+      name: "Develhope",
+      description: "La migliore",
+      location: {
+        city: "Palermo",
+        street: "Via Libertà 58",
+        postalCode: "90139",
+      },
+    },
+  ],
+  "male"
+);
