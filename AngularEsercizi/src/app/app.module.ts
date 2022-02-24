534aad7294847a7b6cdf5d5368dfa40c89906742
Fabio Viola <fabioviola97@gmail.com>
2022-02-25 00:57:42 +0100

    Add ex 3

--- AngularEsercizi/src/app/app.module.ts
+++ AngularEsercizi/src/app/app.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './user/user-container/user-container.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { UserSingleComponent } from './user/user-single/user-single.component';
+import { OrderContainerComponent } from './order/order-container/order-container.component';
+import { OrderListComponent } from './order/order-list/order-list.component';
+import { OrderSingleComponent } from './order/order-single/order-single.component';
+import { OrderDetailComponent } from './order/order-detail/order-detail.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserContainerComponent, UserListComponent, UserDetailComponent, UserSingleComponent, OrderContainerComponent, OrderListComponent, OrderSingleComponent, OrderDetailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
